83e34c079a0071b597aadc101562d389a1f4af1e
Aymen Kh <idev.aymen@gmail.com>
2019-09-02 15:27:30 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,21 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                     #
+#                  SETUP ENVIRONEMENT                 #
+#                                                     #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-# Map ================================================
+
+# Types ===============================================
+
+tiprack_type  = 'tiprack-200ul'
+water_type    = 'point'
+trash_type    = 'point'
+
+tray_type     = '96-PCR-flat'
+petri_type    = 'point'
+
+
+# Maping ==============================================
 
 #   | A2 | B2 | C2 | D2 | E2 |
 #   | A1 | B1 | C1 | D1 | E1 |
@@ -12,16 +28,6 @@ tray_slots    = [ 'A1', 'E1', 'E2', ]
 petri_slots   = [ 'B1', 'D1', 'D2', ]
 
 
-# Variable(s) ========================================
-
-tiprack_type  = 'tiprack-200ul'
-water_type    = 'point'
-trash_type    = 'point'
-
-tray_type     = '96-PCR-flat'
-petri_type    = 'point'
-
-
 # Container(s) ========================================
 
 tipracks = [ containers.load ( tiprack_type , slot ) for slot in tiprack_slots ]
@@ -30,13 +36,12 @@ waters   = [ containers.load ( water_type   , slot ) for slot in water_slots   ]
 petries  = [ containers.load ( petri_type   , slot ) for slot in petri_slots   ]
 trays    = [ containers.load ( tray_type    , slot ) for slot in tray_slots    ]
 
-tiprack = tipracks[0]
 trash   = trashes[0]
+tiprack = tipracks[0]
 water   = waters[0]
-petri   = petries[0]
-tray    = trays[0]
 
-# Pipette(s) ===========================================
+
+# Instruments(s) ======================================
 
 pipette = instruments.Pipette(
 	axis            = 'b',
@@ -46,6 +51,6 @@ pipette = instruments.Pipette(
 	max_volume      = 200,
 	aspirate_speed  = 300,
 	dispense_speed  = 500,
-	tip_racks       = [tiprack],
-	trash_container = trash
+	tip_racks       = tipracks,
+	trash_container = trashes[0]
 )
