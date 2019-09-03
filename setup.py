04051b72c0efedbd063e0976857dc0c18bdc9857
Aymen Kh <idev.aymen@gmail.com>
2019-09-02 21:05:27 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@
 #                                                     #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
+from opentrons import containers, instruments
 
 # Constants ===========================================
 
